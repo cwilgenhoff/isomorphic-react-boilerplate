cc6be0fd4819c27f6db4c6ab5fb1f15a7c8b9814
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2016-08-22 21:51:48 +0100

    Added server rendering and linked bundle

--- server/app.jsx
+++ server/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { renderToString } from 'react-dom/server'
 import { RouterContext, match } from 'react-router';
 import routes from '../shared/routes';
 
@@ -6,15 +7,19 @@ const reactApp = (req, res) => {
   // Tip: https://github.com/reactjs/react-router/blob/master/docs/guides/ServerRendering.md
   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
     if (error) {
-      res.status(500).send(error.message)
-    } else if (redirectLocation) {
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-    } else if (renderProps) {    
+      return res.status(500).send(error.message)
+    }
+
+    if (redirectLocation) {
+      return res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+    }
+
+    if (renderProps) {
       const InitialComponent = (
         <RouterContext {...renderProps} />
       );
       // Tip: https://facebook.github.io/react/docs/top-level-api.html
-      const componentHTML = 'Hello World';
+      const componentHTML = renderToString(InitialComponent);
       const HTML = `
         <!DOCTYPE html>
           <html>
@@ -24,7 +29,7 @@ const reactApp = (req, res) => {
             </head>
             <body>
               <div id="app">${componentHTML}</div>
-              <script type="application/javascript" src=""></script>
+              <script type="application/javascript" src="./bundle.js"></script>
             </body>
         </html>    
       `;
